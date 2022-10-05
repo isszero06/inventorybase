5a62f38712de3efc99d8cdb8bbbbb674769fd084
isszero06 <76986925+isszero06@users.noreply.github.com>
2022-10-05 09:26:29 +0200

    Update __manifest__.py

--- inventorybase/__manifest__.py
+++ inventorybase/__manifest__.py
@@ -41,7 +41,7 @@
     'installable': True,
     'auto_install': False,
     'application': False,
-    'price': 35.0,
+    'price': 25.0,
     'currency': 'USD',
     "pre_init_hook": "pre_init_check",
 }
