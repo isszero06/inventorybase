d0dbad0c303d61e386e0652d2fda7cb457fde728
isszero06 <76986925+isszero06@users.noreply.github.com>
2022-12-01 20:40:39 +0200

    Update __manifest__.py

--- inventorybase/__manifest__.py
+++ inventorybase/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     'name': ' Stock Basic enhancement',
-    'version': '14.0.1',
+    'version': '14.0.2',
     'summary': """Auto Genration for Sales Returns and Purchase Returns operations, 
                 Link them with Sales delivery,
                  Purchase Recipt and other Enhancment""",
@@ -41,7 +41,7 @@
     'installable': True,
     'auto_install': False,
     'application': False,
-    'price': 50.0,
+    'price': 20.0,
     'currency': 'USD',
     "pre_init_hook": "pre_init_check",
 }
